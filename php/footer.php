ff0b151d41c0d749c34ec8e9ad9cf3138e43ea28
pjfin123@gmail.com <pjfin123@gmail.com>
2016-11-14 11:28:41 -0500

    Reduced output font size and fixed broken link

--- php/footer.php
+++ php/footer.php
@@ -4,10 +4,12 @@ echo '
 <footer>
     <span>Copyright © '.date("Y").' <a href="https://simpletoolsonline.com">simpletoolsonline.com</a></span>
     <br><br>
-    <span><a href=".">Home</a></span>
-    <span><a href="contact">Contact</a></span>
-    <span><a href="terms-of-use">Terms Of Use</a></span>
-    <span><a href="privacy-policy">Privacy Policy</a></span>
+    <nav>
+        <span><a href=".">Home</a></span>
+        <span><a href="contact">Contact</a></span>
+        <span><a href="terms-of-use">Terms Of Use</a></span>
+        <span><a href="privacy-policy">Privacy Policy</a></span>
+    </nav>
 </footer>
 </body>
 </html>
